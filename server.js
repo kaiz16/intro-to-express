9b79b8801dbbe3b15eba92d2bace454c7b304d87
kaiz16 <realkaiz7@gmail.com>
2021-04-07 21:26:08 +0800

    added express 2

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-// nada
+const express = require('express')
+// create an instance
+const app = express()
+
+// app.listen
+app.listen(8000, () => {
+    console.log('A server is now listening')
+})
